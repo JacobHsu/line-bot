333c36d38d1eda21932be9b341808704225a04a6
jacobhsu <jacob.hsu.tw@gmail.com>
2018-03-07 21:38:17 +0800

    https://currencylayer.com

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Demo
+ 
+請先加入好友 行動條碼
+
+![alt tag](https://qr-official.line.me/M/X59K430wql.png)
+
+或是手機直接點選 [https://line.me/R/ti/p/%40cfh8893x](https://line.me/R/ti/p/%40cfh8893x)
+
+
 # LINE＠
 
 [admin-official](https://at.line.me/tw/)  
@@ -26,6 +35,14 @@ Webhook URL  `https://jacobhsu-line-bot.herokuapp.com/callback`
 requirements.txt 環境需要那些其他的套件  
 runtime.txt 想要指定的 python 版本  
 
+[requirements.txt](https://devcenter.heroku.com/articles/python-pip)  
+
 # Python 3
 
 [configparser](https://docs.python.org/3/library/configparser.html) — Configuration file parser  
+[Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) is a Python library for pulling data out of HTML and XML files.  
+[PyPI - the Python Package Index](https://pypi.python.org/pypi)  
+
+# API
+
+[currencylayer API](https://currencylayer.com/documentation)  
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, abort
 import configparser
+import requests
+import json
+from bs4 import BeautifulSoup
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -37,9 +40,76 @@ def callback():
     return 'OK'
 
 
+def apple_news():
+    target_url = 'http://www.appledaily.com.tw/realtimenews/section/new/'
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for index, data in enumerate(soup.select('.rtddt a'), 0):
+        if index == 3:
+            return content
+
+        link = data['href']
+        content += '{}\n\n'.format(link)
+    return content
+
+def ptt_hot():
+    target_url = 'http://disp.cc/b/PttHot'
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for index, data in enumerate(soup.select('#list div.row2 div span.listTitle')):
+        if index <= 13:
+            continue
+        title = data.text
+        link = "http://disp.cc/b/" + data.find('a')['href']
+        if data.find('a')['href'] == "796-59l9": #[公告]
+            break
+        content += '{}\n{}\n\n'.format(title, link)
+    return content
+
+def movie():
+    target_url = 'http://www.atmovies.com.tw/movie/next/0/'
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for index, data in enumerate(soup.select('ul.filmNextListAll a')):
+        if index == 5:
+            return content
+        title = data.text.replace('\t', '').replace('\r', '')
+        link = "http://www.atmovies.com.tw" + data['href']
+        content += '{}\n{}\n'.format(title, link)
+    return content
+
+def currencylayer():
+    target_url = 'http://apilayer.net/api/live?access_key='+config['currencylayer']['Access_Key']+'&currencies=TWD,JPY,CNY'
+    response = requests.get(target_url)
+    data = response.text
+    parsed = json.loads(data)
+    rates = parsed['quotes']
+    content = ""
+    for currency, rate in rates.items():
+        content += currency+"="+str(rate)+"\n"
+    return content[:-1]
+
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    content = event.message.text + ' by bot'
+    if event.message.text == "蘋果即時新聞":
+        content = apple_news()
+    elif event.message.text == "近期熱門廢文":  
+        content = ptt_hot()  
+    elif event.message.text == "近期上映電影":  
+        content = movie()
+    elif event.message.text == "今日匯率":  
+        content = currencylayer()        
+    else:
+        content = event.message.text + ' by bot'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=content))
